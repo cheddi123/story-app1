944cd688d3bf5acbdf96624a98e0efb6db612097
Cheddi Charles <cheddi.charles@gmail.com>
2019-08-07 14:21:15 -0500

    Got scrollify to work

--- public/index.js
+++ public/index.js
@@ -1,27 +1,28 @@
-// $(document).ready(function() {
-// 	//Init ScrollSpy
-// 	$('body').scrollspy({ target: '#login' });
+$(document).ready(function() {
+	//Init ScrollSpy
+	$('body').scrollspy({ target: '#main-nav' });
 
-// 	//Add smooth scrolling
-// 	$('#login ').on('click', function(e) {
-// 		// check for hash value
-// 		if (this.hash !== '') {
-// 			// Prevent Default behaviour
-// 			e.preventDefault();
-// 			// Store hash
-// 			const hash = this.hash;
-
-// 			// Animate smooth scroll
-// 			$('html,body').animate(
-// 				{
-// 					scrollTop: $(hash).offset().top,
-// 				},
-// 				2000,
-// 				function() {
-// 					// Add hash to URL after scroll
-// 					window.location.hash = hash;
-// 				}
-// 			);
-// 		}
-// 	});
-// });
+	//Add smooth scrolling
+	$('#main-nav ').on('click',"a", function(e) {
+		// check for hash value
+		if (this.hash !== '') {
+			// Prevent Default behaviour
+			// e.preventDefault();
+			// Store hash
+			const hash = this.hash;
+            //  console.log("click me")
+			// Animate smooth scroll
+			$('html,body').animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				900,
+				function() {
+                  
+					// Add hash to URL after scroll
+					window.location.hash = hash;
+				}
+			);
+		}
+	});
+});
